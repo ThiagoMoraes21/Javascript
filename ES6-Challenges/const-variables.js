4745b61c01a733d0a5058acc7311a6dc436604cd
Thiago Moraes <thiago4x70@gmail.com>
2019-02-28 21:44:48 -0300

    Declaring read-only variables with const

---
+++ ES6-Challenges/const-variables.js
@@ -0,0 +1,20 @@
+// variables declared using const are read-only
+
+"use strict"
+const FAV_PET = "Cats";
+FAV_PET = "Dogs"; // returns error
+
+function printManyTimes(str) {
+    "use strict";
+
+    // change code below this line
+
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i += 2) {
+        console.log(SENTENCE);
+    }
+
+    // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
