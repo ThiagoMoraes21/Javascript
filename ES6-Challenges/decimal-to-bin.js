533c2d81717e8a9a7e8939036e3ec79812187c07
Thiago Moraes <thiago4x70@gmail.com>
2019-03-08 12:03:39 -0300

    Decimal to binary

---
+++ ES6-Challenges/decimal-to-bin.js
@@ -0,0 +1,13 @@
+function convertDecimal(n){
+    let conv = "";
+
+    while(n > 0) {
+        conv += n % 2;
+        n = parseInt(n / 2);
+    }
+
+    return conv.split("").reverse().join();
+
+}
+
+console.log(convertDecimal(156));
