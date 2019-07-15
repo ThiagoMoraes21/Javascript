5ce0231df3506cbb53932a92d6549b83db474382
Thiago Moraes <thiago4x70@gmail.com>
2019-07-15 14:52:56 -0300

    Improve the solution

--- Dates/diff_two_dates.js
+++ Dates/diff_two_dates.js
@@ -23,24 +23,65 @@ const moment = require('moment');
 // var result = hours + ":" + mins + ":" + ((sec > 9) ? sec : ("0" + sec));
 // console.log(result);
 
-function getHours(h) {
-    let dur = moment.duration(h, 'hours');
+// function getHours(h) {
+//     let dur = moment.duration(h, 'hours');
+//     let hours = Math.floor(dur.asHours());
+//     let mins = Math.floor(dur.asMinutes()) - hours * 60;
+//     let sec = Math.floor(dur.asSeconds()) - hours * 60 * 60 - mins * 60;
+
+//     let result = hours + ":" + mins + ":" + ((sec > 9) ? sec : ("0" + sec));
+//     console.log(result);
+//     return result;
+// }
+
+// function getDiff(start, end) {
+//     let diff = end.diff(start, 'hours', true);
+//     console.log('diff: ', diff);
+//     return getHours(diff);
+// }
+
+// let date = 49.75;
+// let start = moment('2019-07-13T18:03:55');
+// let end = moment('2019-07-04T09:15:55');
+// getDiff(start, end);
+
+  getDiff(arr) {
+    let totalSchedule = this.getTotal(arr, "dutyStartsAt", "dutyEndsAt", false);
+    let totalWorked = this.getTotal(arr, "startedAt", "endedAt", true);
+    let signal = Math.max(totalSchedule, totalWorked) == totalSchedule ? "-" : "+";
+    let difference =  totalSchedule - totalWorked;
+
+    return signal + this.getHours(difference);
+  }
+
+  getTotal(arr, start, end, hasCheckin) {
+    return arr.reduce((sum, current) => {
+      let total = 0;
+      
+      if(hasCheckin) {
+        let startDate = current[0]["checkin"][start];
+        let endDate = current[0]["checkin"][end];
+        total = moment(startDate).diff(moment(endDate), "hours", true);
+
+      } else {
+        let startDate = current[0][start];
+        let endDate = current[0][end];
+        total = moment(startDate).diff(moment(endDate), "hours", true);
+      }
+
+      return sum + Math.abs(total);
+    }, 0);
+  }
+
+  getHours(date) {
+    let dur = moment.duration(date, "hours");
     let hours = Math.floor(dur.asHours());
     let mins = Math.floor(dur.asMinutes()) - hours * 60;
-    let sec = Math.floor(dur.asSeconds()) - hours * 60 * 60 - mins * 60;
+    let secs = Math.floor(dur.asSeconds()) - hours * 60 * 60 - mins * 60;
 
-    let result = hours + ":" + mins + ":" + ((sec > 9) ? sec : ("0" + sec));
-    console.log(result);
-    return result;
-}
+    let result = ((hours > 9)  ? hours : ("0" + hours)) + ":"
+               + ((mins  > 9)  ? mins  : ("0" + mins))  + ":" 
+               + ((secs  > 9)  ? secs  : ("0" + secs));
 
-function getDiff(start, end) {
-    let diff = end.diff(start, 'hours', true);
-    console.log('diff: ', diff);
-    return getHours(diff);
-}
-
-let date = 49.75;
-let start = moment('2019-07-13T18:03:55');
-let end = moment('2019-07-04T09:15:55');
-getDiff(start, end);
+    return result; 
+  }
