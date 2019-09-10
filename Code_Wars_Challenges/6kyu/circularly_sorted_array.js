794c3e6186ccbb8b9768568db5f6f23f1a8cd41a
Thiago Moraes <thiago4x70@gmail.com>
2019-09-10 00:17:04 -0300

    Code_wars: circularly sorted array, clever solution

--- Code_Wars_Challenges/6kyu/circularly_sorted_array.js
+++ Code_Wars_Challenges/6kyu/circularly_sorted_array.js
@@ -22,6 +22,7 @@
         [7, 6, 5, 4, 3, 2, 1]
  */
 
+ // brute force
  function isCircleSorted(arr) {
     let max = Math.max(...arr);
     let arr1 = [];
@@ -32,14 +33,24 @@
     }
 
     let concat = arr2.concat(arr1);
-    console.log(arr1, arr2);
-    console.log(concat);
     for(let j = 0; j < concat.length; j++) {
         if(concat[j] > concat[j + 1]) return false;
     }
     return true;
  }
 
+// clever solution
+function isCircleSorted_clever(arr) {
+  let jumped = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[(i + 1) % arr.length]) {
+      if (jumped) return false;
+      jumped = true;
+    }
+  }
+  return true;
+}
+ 
  // tests
 const Test = {
     strictEqual: (output, expected) => {
@@ -48,18 +59,34 @@ const Test = {
     }
 }
  
-// Test.strictEqual(isCircleSorted([2, 3, 4, 5, 6]), true);
-// Test.strictEqual(isCircleSorted([6, 2, 3, 4, 5]), true);
-// Test.strictEqual(isCircleSorted([2, 3, 4, 5, 0, 1]), true);
-// Test.strictEqual(isCircleSorted([4, 5, 6, 9, 1]), true);
-// Test.strictEqual(isCircleSorted([10, 11, 6, 7, 9]), true);
-// Test.strictEqual(isCircleSorted([1, 2, 3, 4, 5]), true);
-// Test.strictEqual(isCircleSorted([5, 7, 43, 987, -9, 0]), true);
-// Test.strictEqual(isCircleSorted([1, 2, 3, 4, 1]), true);
+Test.strictEqual(isCircleSorted([2, 3, 4, 5, 6]), true);
+Test.strictEqual(isCircleSorted([6, 2, 3, 4, 5]), true);
+Test.strictEqual(isCircleSorted([2, 3, 4, 5, 0, 1]), true);
+Test.strictEqual(isCircleSorted([4, 5, 6, 9, 1]), true);
+Test.strictEqual(isCircleSorted([10, 11, 6, 7, 9]), true);
+Test.strictEqual(isCircleSorted([1, 2, 3, 4, 5]), true);
+Test.strictEqual(isCircleSorted([5, 7, 43, 987, -9, 0]), true);
+Test.strictEqual(isCircleSorted([1, 2, 3, 4, 1]), true);
 
-// Test.strictEqual(isCircleSorted([3, 2, 4, 5, 6]), false);
-// Test.strictEqual(isCircleSorted([4, 1, 2, 5]), false);
-// Test.strictEqual(isCircleSorted([8, 7, 6, 5, 4, 3]), false);
-// Test.strictEqual(isCircleSorted([6, 7, 4, 8]), false);
-// Test.strictEqual(isCircleSorted([7, 6, 5, 4, 3, 2, 1]), false);
+Test.strictEqual(isCircleSorted([3, 2, 4, 5, 6]), false);
+Test.strictEqual(isCircleSorted([4, 1, 2, 5]), false);
+Test.strictEqual(isCircleSorted([8, 7, 6, 5, 4, 3]), false);
+Test.strictEqual(isCircleSorted([6, 7, 4, 8]), false);
+Test.strictEqual(isCircleSorted([7, 6, 5, 4, 3, 2, 1]), false);
 Test.strictEqual(isCircleSorted([2, 3, 4, 4, 6, 6, -8, -3, 0, 1]), true);
+
+Test.strictEqual(isCircleSorted_clever([2, 3, 4, 5, 6]), true);
+Test.strictEqual(isCircleSorted_clever([6, 2, 3, 4, 5]), true);
+Test.strictEqual(isCircleSorted_clever([2, 3, 4, 5, 0, 1]), true);
+Test.strictEqual(isCircleSorted_clever([4, 5, 6, 9, 1]), true);
+Test.strictEqual(isCircleSorted_clever([10, 11, 6, 7, 9]), true);
+Test.strictEqual(isCircleSorted_clever([1, 2, 3, 4, 5]), true);
+Test.strictEqual(isCircleSorted_clever([5, 7, 43, 987, -9, 0]), true);
+Test.strictEqual(isCircleSorted_clever([1, 2, 3, 4, 1]), true);
+
+Test.strictEqual(isCircleSorted_clever([3, 2, 4, 5, 6]), false);
+Test.strictEqual(isCircleSorted_clever([4, 1, 2, 5]), false);
+Test.strictEqual(isCircleSorted_clever([8, 7, 6, 5, 4, 3]), false);
+Test.strictEqual(isCircleSorted_clever([6, 7, 4, 8]), false);
+Test.strictEqual(isCircleSorted_clever([7, 6, 5, 4, 3, 2, 1]), false);
+Test.strictEqual(isCircleSorted_clever([2, 3, 4, 4, 6, 6, -8, -3, 0, 1]), true);
